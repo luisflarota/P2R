647d9fb2edd32a4e193c317b2059cf28bf689652
luisflarota <lflarota@uc.cl>
2022-03-20 14:00:58 -0600

    First Commit - Adding files

---
+++ backCopy.py
@@ -0,0 +1,255 @@
+import ast
+import math
+import string
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def read_csvfile(file,step_intmax):
+    location_p = {'Start': [471, 3],
+                'Entrance': [343, 58],
+                'Stock1': [354, 287],
+                'Stock2': [364, 386],
+                'Stock3': [252, 422],
+                'Stock4': [132, 169],
+                'Scale': [290, 77],
+                'Shovel': [109, 345]}
+    set_of_stocks = [loc for loc in location_p if 'Stock' in loc]            
+    raw_data= pd.read_csv(file)
+    segment, points = filter_data(raw_data,'PeteLien.png')
+    out = delete_doubles(segment)    
+    letter = string.ascii_lowercase
+    letter = list(letter)
+    nodes = {}
+    new_out =[]
+    for let_i, segment in enumerate(np.unique(out[:,2])):
+        let = letter[let_i]
+        data_segm =out[out[:,2]==segment]
+        for seg_i,c_data_segm in enumerate(data_segm):
+            val_data_segm = list(c_data_segm[:2])
+            if val_data_segm in nodes.values():
+                x = [k for k,v in nodes.items() if v == val_data_segm]
+                new_out.append(list(data_segm[seg_i])+[x[0]])
+                continue
+            nodes[let+str(seg_i)] = val_data_segm
+            new_out.append(list(data_segm[seg_i])+[let+str(seg_i)])
+    new_out = pd.DataFrame(new_out, columns = ['x','y','seg','node'])
+    graph ={}
+    for seg_n in np.unique(new_out['seg']):
+        data_seg_out = np.array(new_out[new_out['seg']== seg_n])
+        for i_dato in range(data_seg_out.shape[0]-1):
+            bef_p = data_seg_out[i_dato][:2]
+            node_bef = find_key(bef_p, nodes)
+            aft_p = data_seg_out[i_dato+1][:2]
+            node_aft = find_key(aft_p, nodes)
+            name_node =node_bef+node_aft
+            res_bef_aft  = aft_p-bef_p
+            distance = np.sqrt(np.sum(np.square(res_bef_aft)))
+            step = step_intmax
+            if distance<step_intmax:
+                step =int(distance/2)+1
+            azimuth = math.atan2(res_bef_aft[1], res_bef_aft[0])
+            points_i = [[bef_p[0] + st*math.cos(azimuth),bef_p[1] + st*math.sin(azimuth)]
+                        for st in range(0, round(distance), step)]
+            points_i.append(list(aft_p))
+            distances_graph = [get_diference(bef_p,aft_p)]
+            #print(node_bef, node_aft, len(points_i), node_bef+node_aft)
+            dis_acum = 0
+            for st in range(0, round(distance-step), step):
+                node_na_before = name_node+str(st)
+                if st == 0:
+                    node_na_before = node_bef
+                if node_na_before not in graph:
+                    graph[node_na_before] = list()
+                node_na_final  = name_node+str(st+step)
+                x_new = bef_p[0] + (st+1)*math.cos(azimuth)
+                y_new = bef_p[1] + (st+1)*math.sin(azimuth)
+                if node_na_final not in nodes:
+                    nodes[node_na_final] = [x_new, y_new]
+                graph[node_na_before].append((node_na_final, step))
+                dis_acum+=step
+                if st+step>= round(distance-step):
+                    node_na_before = name_node+str(st+step)
+                    node_na_final = node_aft
+                    if node_na_before not in graph:
+                        graph[node_na_before] = list()
+                    graph[node_na_before].append((node_na_final, distance-dis_acum))
+    
+    nodes_loc = {key_lp:node_lp for key_lp in location_p for node_lp in nodes if location_p[key_lp] == nodes[node_lp]}
+
+    #new_out represents nodes
+    return new_out, nodes, graph, nodes_loc, location_p, set_of_stocks,set_of_stocks
+def shape_matrixmom_sec(dictionary, decision_time,p_customer, p_shovel, linewidth, N_Simulations, set_stocks, unassigned = [0,0,0]):
+    change_stockpiles ={}
+    matrix_for_customer = np.full((2**2**4,N_Simulations,2),None)
+    matrix_for_shovel = np.empty(shape = (0,2), dtype=object)
+    unassigned = [0,0,0]
+    costumer_palette = np.full((2**2**4,N_Simulations,3),unassigned, dtype=float)
+    costumer_linew = np.full((2**2**4,N_Simulations,),0)
+    shovel_palette= np.empty(shape = (0,3), dtype=float)
+    shovel_linew = np.empty(shape = (0,), dtype=float)
+    stock_before = 0
+    for index, key in enumerate(dictionary):
+        customer_data = dictionary[key]
+        to_entrance= [[None, None] for x in range(np.sum(decision_time[:index+1]))]+customer_data[0]
+        to_stock = customer_data[1]
+        to_end = customer_data[2]
+        sho_to_sho = customer_data[3]
+        if index == 0:
+            none_shovel = [[None, None] for x in range(len(to_entrance+to_stock) - len(sho_to_sho))]
+            sho_to_sho = none_shovel+sho_to_sho
+            stock_before= len(to_stock)
+        else:
+            len_add_stock = stock_before - decision_time[index] +len(sho_to_sho) - len(to_stock)
+            if len_add_stock> 0:
+                to_stock = [[None, None] for x in range(len_add_stock)]+ to_stock
+                stock_before= len(to_stock)
+            else:
+                len_add_stock = len_add_stock*-1
+                sho_to_sho = sho_to_sho + [[None, None] for x in range(len_add_stock)]
+                stock_before = len(to_stock)
+        if index == len(dictionary)-1:
+            large = len(to_entrance+to_stock+to_end)
+        #change stockpiles
+        
+        matrix_for_customer[:,index][0:len(to_entrance+to_stock+to_end)] = np.array(to_entrance+to_stock+to_end)
+        costumer_palette[:,index][0:len(to_entrance+to_stock+to_end)] = np.array([p_customer[
+            index]for x in range(len(to_entrance+to_stock+to_end))])
+        costumer_linew[:,index][0:len(to_entrance+to_stock+to_end)] = np.array([linewidth[
+            index]for x in range(len(to_entrance+to_stock+to_end))])
+        matrix_for_shovel = np.concatenate([matrix_for_shovel,np.array(sho_to_sho)])
+        change_stockpiles[(set_stocks[index], index)] = len(matrix_for_shovel)
+        shovel_palette= np.concatenate([shovel_palette,np.array([p_shovel[
+            index]for x in range(len(sho_to_sho))])])
+        shovel_linew= np.concatenate([shovel_linew,np.array([linewidth[
+            index]for x in range(len(sho_to_sho))])])
+    return matrix_for_customer, matrix_for_shovel, costumer_palette,costumer_linew,shovel_palette,shovel_linew,large,change_stockpiles
+
+
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+def filter_data(data, filename):
+    list_segments = []
+    list_points=[]
+    data_filter = data[data['filename']==filename]['region_shape_attributes']
+    date_name = data[data['filename']==filename]['region_attributes']
+    for dataf, datan in zip(enumerate(data_filter),enumerate(date_name)):
+        index = dataf[0]
+        data_r = dataf[1]
+        dicti = ast.literal_eval(data_r)
+        #print(dicti)
+        if 'polyline' in dicti.values():
+            x_points = dicti['all_points_x']
+            y_points = dicti['all_points_y']
+            list_segments+=[[x,y, index]for x,y in zip(x_points, y_points)]
+        else:
+            dict_n = ast.literal_eval(datan[1])
+            x_a = dicti['cx']
+            y_a= dicti['cy']
+            name = dict_n['name']
+            list_points.append([x_a, y_a, index,name])
+    return np.array(list_segments),np.array(list_points)
+
+def delete_doubles(array):
+    new = array
+    #print(array.shape)
+    for index,coordinate in enumerate(array):
+        new_array = np.array([np.linalg.norm(x) for x in array-coordinate])
+        index_2   = np.argwhere((new_array>0)&(new_array<7))
+        if len(index_2)>0:
+            for ind_2 in index_2:
+                new[index][0] =new[ind_2][0][0]
+                new[index][1] =new[ind_2][0][1]
+    return new    
+def get_diference(a,b):
+    a = np.array(a)
+    b = np.array(b)
+    delta = b-a
+    distance= np.square(delta)
+    return np.sqrt(np.sum(distance))
+def find_key(val, diction):
+    val = list(val)
+    x = [k for k,v in diction.items() if v == val]
+    return x[0]       
+def calculateAP(path, graph):
+    #path = ['S','A','B']
+    #len = 3 - 1 = 2
+    acumulatedPath = 0
+    
+    for position,item in enumerate(path):
+        if position < len(path) - 1:
+            for k,v in graph.items():
+                if item == k:
+                    expand = dict(v) #[('A',7),('B',2),('C',3)]
+                    acumulatedPath += expand[path[position + 1]]
+                    
+    return acumulatedPath
+def loadQueue(graph,start):
+    
+    #imoprts
+    import math
+    
+    queue = []
+    for item in graph:
+        if(item == start):
+            queue.append([item,[item],0])
+        else:
+            queue.append([item,[],math.inf])
+    
+    return queue
+def Dijkstra(graph,start,end):
+    
+    #Imports
+    import math
+    from operator import itemgetter
+
+    #Iniciar variaveis
+    #queue = [(node,caminho[],valorCaminhoAcumulado)]
+    queue = loadQueue(graph,start)
+    done = []
+    
+    count = 1
+    
+    while queue:
+        
+        head = queue[0]  #['S',[c],ca]
+        rest = queue[1:] #[ [n,[c],ca], [n,[c],ca], [n,[c],ca], [n,[c],ca] ]
+        
+        if head[0] == end:
+
+            break
+            
+        else:
+            #Fazer expand
+            #Ex: expand = ['A','B','C']
+            #Ex:  h = ('S',caminho['S'],0)
+            #Ex: h[1] = ['S','A']
+            expand = [i[0] for i in graph[head[0]]] #Ex:['A','B','C']
+            
+            #Ciclo para iterar os nós da queue
+            for position,(n,c,v) in enumerate(queue):
+                
+                #Ver se o nó atual faz parte do expand
+                if n in expand:
+                    
+                    #Arranjar a path para este né e o seu custo acumulado
+                    path = head[1] + [n] #['S','A'] + ['B'] = ['S','A','B']
+                    acumulatedPath = calculateAP(path, graph) #7
+                    
+                    #Se o caminho acumulado for inferior ao caminho antigo, substitui-lo
+                    if acumulatedPath < v:
+                        queue[position] = ([n,path,acumulatedPath])
+            
+            #Remover o head da Queue e adiciona-lo ao Done. Fazer sort da Queue utilizando o elemnto [2] como parametro de comparação
+            queue.remove(head)
+            queue = sorted(queue,key=itemgetter(2))
+            done.append(head)
+            
+            count += 1
+
+    return head
---
+++ trial_errorCopy.py
@@ -0,0 +1,182 @@
+import ast
+import json
+from itertools import count
+from tkinter import N
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+#from celluloid import Camera
+from matplotlib import animation
+from matplotlib.axes._axes import _log as matplotlib_axes_logger
+
+from backCopy import *
+
+#from matplotlib.patches import Circle
+
+
+matplotlib_axes_logger.setLevel('ERROR')
+plt.rcParams['animation.ffmpeg_path'] = r"C:\\Users\\101114992\\Documents\\Research\\98Coding\\ffmpeg\\bin\\ffmpeg.exe"
+#----------------------------
+#TRASH FOR NOW
+# shovel_payload = 10 #tons
+# cicle_shovel = 20 #seconds
+# truck_payload = 60 #tons
+# time_load = (truck_payload/shovel_payload)*cicle_shovel
+#----------------------------
+
+#----------PLACES:
+STEP_INTERPOLATION = 20
+N_Simulations = 2
+truck_payload = 50 #tons
+palette = sns.color_palette(None, 40)
+master = read_csvfile('last_nodeshovel_1.csv', STEP_INTERPOLATION)
+nodes_master = master[0]
+nodes = master[1]
+graph = master[2]
+fixed_location_b = master[3]
+fixed_location = master[4]
+location_piles = {key:fixed_location[key]+\
+    [np.random.randint(truck_payload*(N_Simulations+1), 300)]+[palette[index+2]]\
+     for index,key in enumerate(fixed_location) if 'tock' in key}
+print(location_piles)
+set_of_stocks = master[5]
+SHOVEL_NODE = fixed_location_b['Shovel']   
+end ='Scale'
+entrance = 'Entrance'
+#---------- NUMBER OF SIMULATIONS:
+from_w = ['Start' for n_sim in range(N_Simulations)]
+decision_time = [0]+[np.random.randint(15,40) for n_sim in range(N_Simulations-1)]
+to_w =[set_of_stocks[np.random.randint(0, len(set_of_stocks))] for i in range(N_Simulations)]
+
+print(to_w)
+
+fig, (ax, ax1) = plt.subplots(1,2,figsize=(15,7))
+palette_customer = sns.color_palette("husl",n_colors=N_Simulations)
+palette_shovel = sns.color_palette("coolwarm",n_colors=N_Simulations)
+linewidth = [i for i in range(N_Simulations*2,0,-2)]
+#---------- CUSTOMERS' TRUCK PAYLOAD/ THIS WILL DECIDE HOW FAST THE TRUCKS AND SHOVEL WILL LOAD
+
+
+
+#getting location of entrance + plotting radius
+radius_entrance = 50
+#circle_entrance = Circle(tuple(fixed_location['Entrance']), radius_entrance, color='b',fill=False, hatch= '+')
+chart = ax.scatter([],[])
+chart = ax1.scatter([],[])
+I = cv2.imread('backg.png')
+ax.imshow(I,cmap="gray")
+ax1.imshow(I,cmap="gray")
+def ini():
+    for segment in np.unique(nodes_master['seg']):
+        sample = np.array(nodes_master[nodes_master['seg']==segment][['x','y']])
+        for x1, x2 in zip(sample[:-1],sample[1:]):
+            ax.annotate('',xy=(x2[0],x2[1]), xytext=(x1[0],x1[1]), arrowprops=dict(
+                arrowstyle='->', linestyle="--", lw=2, color='#ccffe7'))
+            ax1.annotate('',xy=(x2[0],x2[1]), xytext=(x1[0],x1[1]), arrowprops=dict(
+                arrowstyle='->', linestyle="--", lw=2, color='#ccffe7'))    
+    for index,  location in enumerate(fixed_location):
+        coordinate =fixed_location[location]
+        size = 500
+        marker = 'x'
+        if 'tock' not in location:
+            ax.scatter(coordinate[0],coordinate[1], label = location, marker=marker, s = size, linewidths=5, c= np.array(palette[index]))
+            ax1.scatter(coordinate[0],coordinate[1], label = location, marker=marker, s = size, linewidths=5, c= np.array(palette[index]))
+            ax.text(coordinate[0]+10,coordinate[1]+10, location, c= np.array(palette[index]), fontsize =12, backgroundcolor='white')
+            ax1.text(coordinate[0]+10,coordinate[1]+10, location, c= np.array(palette[index]), fontsize =12, backgroundcolor='white')
+        # if location == 'Entrance':
+        #     ax.add_patch(circle_entrance)
+            #ax1.add_patch(circle_entrance)
+    ax.text(100,100, to_w)
+    ax1.text(100,100, to_w)
+    plt.gca().set_aspect('equal', adjustable='box')
+    #plt.legend(loc='best',prop={'size': 15})
+    plt.tight_layout()
+    return chart
+
+
+
+#---------- STARTING SIMULATION
+dict_discretize = dict()
+i=1
+for where, to  in zip(from_w,to_w):
+    val_from_w = fixed_location_b[where]
+    val_to_entrance = fixed_location_b[entrance]
+    val_to_w = fixed_location_b[to]
+    val_end = fixed_location_b[end]
+    shortest_toentrace = Dijkstra(graph,val_from_w, val_to_entrance)
+    shortest_path = Dijkstra(graph,val_to_entrance, val_to_w)
+    shortest_end = Dijkstra(graph,val_to_w, val_end)
+    shovel_path = Dijkstra(graph,SHOVEL_NODE, val_to_w)
+    points_toentrance = np.array([nodes[val_shor] for val_shor in shortest_toentrace[1]]).tolist()
+    points_customer_shortest =np.array([nodes[val_shor] for val_shor in shortest_path[1]]).tolist()
+    points_customer_end =np.array([nodes[val_shor] for val_shor in shortest_end[1]]).tolist()
+    points_shovel_shortest = np.array([nodes[val_shor] for val_shor in shovel_path[1]]).tolist()
+    SHOVEL_NODE = val_to_w
+    dict_discretize[where+str(i)] = [points_toentrance, points_customer_shortest,points_customer_end,points_shovel_shortest]
+    i+=1 
+
+
+matrix_custo, matrix_sho,\
+    costumer_palette,costumer_linew,shovel_palette,\
+        shovel_linew,large,set_stocks = shape_matrixmom_sec(dict_discretize, decision_time,palette_customer,
+        palette_shovel, linewidth,N_Simulations,to_w)
+
+#important to plot stockpiles
+array_stockpiles = np.array(list(location_piles.values()),dtype=object)
+print(array_stockpiles)
+
+
+
+def animate(i):
+
+     
+    # print(costumer_palette[i])
+    # print(shovel_palette[i])
+    #try:
+     #   if i< matrix_custo.shape[0]-1:
+    #---------------------------%% THIS SHOULD NOT BE A FIXED PARAMETER!!!!!
+    if i in set_stocks.values():
+        stock = [key for key,value in set_stocks.items() if value == i][0][0]
+        location_piles[stock][2] = location_piles[stock][2] - truck_payload
+
+    size_stock = list(np.array(list(location_piles.values()),dtype=object)[:,2])
+    print(size_stock)
+    text_stok = [key+' {} tons'.format(values[2]) for key,values in location_piles.items()]
+    ax.scatter(array_stockpiles[:,0],array_stockpiles[:,1], marker='o', s = size_stock, c= array_stockpiles[:,3])
+    [ax.text(location_piles[key][0]+10,location_piles[key][1]+10, \
+        text_stok[index], c= location_piles[key][3], fontsize =12, backgroundcolor='white')\
+            for index,key in enumerate(location_piles)]
+        
+    if i == large+1:
+        exit()
+    try:
+        ax.scatter(matrix_custo[i][:,0],matrix_custo[i][:,1], c=costumer_palette[i], linewidths=costumer_linew[i])
+    except:
+        return chart
+    
+    try:
+        ax1.scatter(matrix_sho[i][0],matrix_sho[i][1], c=shovel_palette[i], linewidths=shovel_linew[i])
+    except:
+        return chart        
+    #except:
+    #    return
+    
+    return chart
+        
+    
+
+
+ani = animation.FuncAnimation(fig, animate, init_func = ini,frames = 2000, interval = 190, repeat=False)
+#ani.save('Sunday.mp4')
+plt.show()
+#f = "C:/Users/101114992/Documents/Research/98Coding/animation.mp4" 
+#writermp4 = animation.FFMpegWriter(fps=1000) 
+#initial_time = time.time()
+#print(initial_time)
+#ani.save(f, writer=writermp4)
+#end_time = time.time()
+
+
